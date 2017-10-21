4bd8dfb3db37e12fb3f8937e731e401946786539
NicolasGripont <Nicolas.Gripont@insa-lyon.fr>
2017-10-20 20:02:58 -0400

    indentation

--- TP3/assets/scripts/controllers/ProductsController.js
+++ TP3/assets/scripts/controllers/ProductsController.js
@@ -1,52 +1,52 @@
 "use strict";
 
 function ProductsController(model, view) {
-    this._model = model;
-    this._view = view;
-    var _this = this;
+  this._model = model;
+  this._view = view;
+  var _this = this;
 
 
-    this._view.criteriaButtonClickedEvent.attach(function (sender, args) {
-        _this.sortProducts(args.criteria, args.orderBy);
-    });
+  this._view.criteriaButtonClickedEvent.attach(function (sender, args) {
+      _this.sortProducts(args.criteria, args.orderBy);
+  });
 
-    this._view.categoryButtonClickedEvent.attach(function (sender, args) {
-        _this.filterProducts(args.category);
-    });
+  this._view.categoryButtonClickedEvent.attach(function (sender, args) {
+      _this.filterProducts(args.category);
+  });
 }
 
 ProductsController.prototype = {
-    sortProducts : function (criteria, orderBy) {
-        if(criteria && orderBy) {
-            this._model.sort(criteria,orderBy);
-        }
-    },
-    
-    filterProducts : function (category) {
-        if(category) {
-            this._model.filter(category);
-        }
-    }, 
+  sortProducts : function (criteria, orderBy) {
+      if(criteria && orderBy) {
+          this._model.sort(criteria,orderBy);
+      }
+  },
+  
+  filterProducts : function (category) {
+      if(category) {
+          this._model.filter(category);
+      }
+  }, 
 
-    loadData : function () {
-      var self = this;
-      $.ajax({
-        url: "./data/products.json",
-        type: "GET",
-        dataType : "json"
-      })
-      .done(function(json) {
-        self._model.resetProducts(json);
-        localStorage.setItem("products-list",JSON.stringify(json));
-      })
-      .fail(function( xhr, status, errorThrown ) {
-        console.log( "Error: " + errorThrown );
-        console.log( "Status: " + status );
-        console.dir( xhr );
-        localStorage.setItem("products-list","");
-        self._view.showError();
-      });
-    }
+  loadData : function () {
+    var self = this;
+    $.ajax({
+      url: "./data/products.json",
+      type: "GET",
+      dataType : "json"
+    })
+    .done(function(json) {
+      self._model.resetProducts(json);
+      localStorage.setItem("products-list",JSON.stringify(json));
+    })
+    .fail(function( xhr, status, errorThrown ) {
+      console.log( "Error: " + errorThrown );
+      console.log( "Status: " + status );
+      console.dir( xhr );
+      localStorage.setItem("products-list","");
+      self._view.showError();
+    });
+  }
 };
 
 /* MAIN */
--- TP3/assets/scripts/views/ProductsView.js
+++ TP3/assets/scripts/views/ProductsView.js
@@ -1,85 +1,83 @@
 "use strict";
 
 function ProductsView(model, elements) {
-    this._model = model;
-    this._elements = elements;
+  this._model = model;
+  this._elements = elements;
 
-    this.categoryButtonClickedEvent = new Event(this);
-    this.criteriaButtonClickedEvent = new Event(this);
+  this.categoryButtonClickedEvent = new Event(this);
+  this.criteriaButtonClickedEvent = new Event(this);
 
-    var _this = this;
+  var _this = this;
 
-    // attach model listeners
-    this._model.currentProductsChangedEvent.attach(function () {
-      _this.rebuildProducts();
-    });
+  // attach model listeners
+  this._model.currentProductsChangedEvent.attach(function () {
+    _this.rebuildProducts();
+  });
 
-    // attach listeners to HTML controls
-    $.each(this._elements.categoriesButtonsGroups.children(), function(i, button) {
-      $(button).click(function(e) {
-        _this.unselectButton(e.target);
-        _this.selectButton(e.target);
-        _this.categoryButtonClickedEvent.notify({category: $(e.target).attr("category")});
-      })
-    });
+  // attach listeners to HTML controls
+  $.each(this._elements.categoriesButtonsGroups.children(), function(i, button) {
+    $(button).click(function(e) {
+      _this.unselectButton(e.target);
+      _this.selectButton(e.target);
+      _this.categoryButtonClickedEvent.notify({category: $(e.target).attr("category")});
+    })
+  });
 
-    $.each(this._elements.criteriaButtonsGroups.children(), function(i, button) {
-      $(button).click(function(e) {
-        _this.unselectButton(e.target);
-        _this.selectButton(e.target);
-        _this.criteriaButtonClickedEvent.notify({criteria: $(e.target).attr("criteria"), orderBy: $(e.target).attr("orderBy")});
-      })
-    });
-
-
+  $.each(this._elements.criteriaButtonsGroups.children(), function(i, button) {
+    $(button).click(function(e) {
+      _this.unselectButton(e.target);
+      _this.selectButton(e.target);
+      _this.criteriaButtonClickedEvent.notify({criteria: $(e.target).attr("criteria"), orderBy: $(e.target).attr("orderBy")});
+    })
+  });
 };
 
 ProductsView.prototype = {
-    show : function () {
-        this.rebuildProducts();
-    },
+  show : function () {
+      this.rebuildProducts();
+  },
 
-    unselectButton : function (button) {
-      $.each($(button).parent().children(), function(i, item) {
-        $(item).removeClass("selected");
+  unselectButton : function (button) {
+    $.each($(button).parent().children(), function(i, item) {
+      $(item).removeClass("selected");
+    });
+  },
+  
+  selectButton : function (button) {
+    $(button).addClass("selected");
+  },
+
+  rebuildProducts : function () {
+    var _this = this
+    var products, productsList, productsCount;
+
+    productsList = this._elements.productsList;
+    productsCount = this._elements.productsCount;
+    productsList.empty();
+
+    products = this._model.getProducts();
+    if(products) {
+      $.each(products, function(i, item) {
+        productsList.append(_this.createProductHtml(item));
       });
-    },
-    
-    selectButton : function (button) {
-      $(button).addClass("selected");
-    },
-
-    rebuildProducts : function () {
-      var _this = this
-      var products, productsList, productsCount;
-
-      productsList = this._elements.productsList;
-      productsCount = this._elements.productsCount;
-      productsList.empty();
-
-      products = this._model.getProducts();
-      if(products) {
-        $.each(products, function(i, item) {
-          productsList.append(_this.createProductHtml(item));
-        });
-        productsCount.html(products.length + " produits");
-      }
-    },
-
-    createProductHtml : function (jsonProduct) {
-      const produit = '<div class="product">' +
-                      '  <a href="product.html?id='+ jsonProduct["id"] +'" title="En savoir plus...">' +
-                      '    <h2>' + jsonProduct["name"] + '</h2>' +
-                      '    <img src="./assets/img/' + jsonProduct["image"] + '" alt="' + jsonProduct["image"] + '"/>' +
-                      '    <p class="product-price">' + jsonProduct["price"].toString().replace('.',",") + '&thinsp;$</p>' +
-                      '  </a>' +
-                      '</div>';
-      return produit;
-    }, 
-
-    showError : function() {
-      $("#products-list").html("Une erreur est survenue lors du chargement des produits...")
+      productsCount.html(products.length + " produits");
     }
+  },
+
+  createProductHtml : function (jsonProduct) {
+    const produit = '<div class="product">' +
+                    '  <a href="product.html?id='+ jsonProduct["id"] +'" title="En savoir plus...">' +
+                    '    <h2>' + jsonProduct["name"] + '</h2>' +
+                    '    <img src="./assets/img/' + jsonProduct["image"] + '" alt="' + jsonProduct["image"] + '"/>' +
+                    '    <p class="product-price">' + jsonProduct["price"].toString().replace('.',",") + '&thinsp;$</p>' +
+                    '  </a>' +
+                    '</div>';
+    return produit;
+  }, 
+
+  showError : function() {
+    $("#products-list").html("Une erreur est survenue lors du chargement des produits...")
+  }
 };
 
 
