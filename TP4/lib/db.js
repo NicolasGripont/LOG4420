9294d5a99d6452b7fa14527aaea782077d740e1b
KaiserCroc <piebay13@laposte.net>
2017-11-03 14:28:35 -0400

    Ajout lien bd

--- TP4/lib/db.js
+++ TP4/lib/db.js
@@ -28,5 +28,16 @@ mongoose.model("Product", Product);
 
 mongoose.Promise = global.Promise;
 
-// TODO: Initialiser la connexion avec le "connect string" de votre base de données.
-//mongoose.connect("mongodb://...", { useMongoClient: true });
+var config = require('../config.json');
+
+
+// DB connexion
+var dbConfig = config.database;
+var dbOptions = {useMongoClient: true};
+mongoose.connect('mongodb://' + dbConfig.username + ':' + dbConfig.password + '@' +
+  dbConfig.host + ':' + dbConfig.port + '/' + dbConfig.name, dbOptions, function (error) {
+
+  if(error) { //TODO
+    console.log("Database error when mongoose.connect : ", error);
+  }
+});
