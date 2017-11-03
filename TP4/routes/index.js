4758be0917b7eb0b5a7f03f6d59a1310f8f6b88d
KaiserCroc <piebay13@laposte.net>
2017-11-03 13:19:10 -0400

    Add des routes

--- TP4/routes/index.js
+++ TP4/routes/index.js
@@ -5,4 +5,32 @@ router.get("/", function(req, res) {
   res.render("index", { title: "Accueil", message: "Ça semble fonctionner!" });
 });
 
+router.get("/accueil", function(req, res) {
+  res.render("index", { title: "Accueil", message: "Ça semble fonctionner!" });
+});
+
+router.get("/produits", function(req, res) {
+  res.render("produits", { title: "Accueil", message: "Ça semble fonctionner!" });
+});
+
+router.get("/produit", function(req, res) {
+  res.render("produit", { title: "Accueil", message: "Ça semble fonctionner!" });
+});
+
+router.get("/contact", function(req, res) {
+  res.render("contact", { title: "Accueil", message: "Ça semble fonctionner!" });
+});
+router.get("/panier", function(req, res) {
+  res.render("panier", { title: "Accueil", message: "Ça semble fonctionner!" });
+});
+
+router.get("/commande", function(req, res) {
+  res.render("commande", { title: "Accueil", message: "Ça semble fonctionner!" });
+});
+router.get("/confirmation", function(req, res) {
+  res.render("confirmation", { title: "Accueil", message: "Ça semble fonctionner!" });
+});
+
+
+
 module.exports = router;
