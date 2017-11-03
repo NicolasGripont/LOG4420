e30bbcbd47360738d1cb9a23aa79d67eb2f63260
KaiserCroc <piebay13@laposte.net>
2017-11-03 13:27:17 -0400

    Modif route add title

--- TP4/routes/index.js
+++ TP4/routes/index.js
@@ -2,33 +2,33 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res) {
-  res.render("index", { title: "Accueil", message: "Ça semble fonctionner!" });
+  res.render("index", { title: "OnlineShop - Accueil" });
 });
 
 router.get("/accueil", function(req, res) {
-  res.render("index", { title: "Accueil", message: "Ça semble fonctionner!" });
+  res.render("index", { title: "OnlineShop - Accueil"});
 });
 
 router.get("/produits", function(req, res) {
-  res.render("products", { title: "Accueil", message: "Ça semble fonctionner!" });
+  res.render("products", { title: "OnlineShop - Produits"});
 });
 
 router.get("/produit", function(req, res) {
-  res.render("product", { title: "Accueil", message: "Ça semble fonctionner!" });
+  res.render("product", { title: "OnlineShop - Produit"});
 });
 
 router.get("/contact", function(req, res) {
-  res.render("contact", { title: "Accueil", message: "Ça semble fonctionner!" });
+  res.render("contact", { title: "OnlineShop - Contact"});
 });
 router.get("/panier", function(req, res) {
-  res.render("shopping-cart", { title: "Accueil", message: "Ça semble fonctionner!" });
+  res.render("shopping-cart", { title: "OnlineShop - Panier"});
 });
 
 router.get("/commande", function(req, res) {
-  res.render("order", { title: "Accueil", message: "Ça semble fonctionner!" });
+  res.render("order", { title: "OnlineShop - Commande"});
 });
 router.get("/confirmation", function(req, res) {
-  res.render("confirmation", { title: "Accueil", message: "Ça semble fonctionner!" });
+  res.render("confirmation", { title: "OnlineShop - Confirmation"});
 });
 
 
