1c2b883328a78ac610420146c0842b838a50ea0c
NicolasGripont <Nicolas.Gripont@insa-lyon.fr>
2017-11-03 14:00:23 -0400

    mise à jour header

--- TP4/routes/index.js
+++ TP4/routes/index.js
@@ -2,33 +2,33 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res) {
-  res.render("index", { title: "OnlineShop - Accueil" });
+  res.render("index", { title: "OnlineShop - Accueil", activeLi: "Accueil"});
 });
 
 router.get("/accueil", function(req, res) {
-  res.render("index", { title: "OnlineShop - Accueil"});
+  res.render("index", { title: "OnlineShop - Accueil", activeLi: "Accueil"});
 });
 
 router.get("/produits", function(req, res) {
-  res.render("products", { title: "OnlineShop - Produits"});
+  res.render("products", { title: "OnlineShop - Produits", activeLi: "Produits"});
 });
 
 router.get("/produit", function(req, res) {
-  res.render("product", { title: "OnlineShop - Produit"});
+  res.render("product", { title: "OnlineShop - Produit", activeLi: "Produits"});
 });
 
 router.get("/contact", function(req, res) {
-  res.render("contact", { title: "OnlineShop - Contact"});
+  res.render("contact", { title: "OnlineShop - Contact", activeLi: "Contact"});
 });
 router.get("/panier", function(req, res) {
-  res.render("shopping-cart", { title: "OnlineShop - Panier"});
+  res.render("shopping-cart", { title: "OnlineShop - Panier", activeLi: "Panier"});
 });
 
 router.get("/commande", function(req, res) {
-  res.render("order", { title: "OnlineShop - Commande"});
+  res.render("order", { title: "OnlineShop - Commande", activeLi: "Panier"});
 });
 router.get("/confirmation", function(req, res) {
-  res.render("confirmation", { title: "OnlineShop - Confirmation"});
+  res.render("confirmation", { title: "OnlineShop - Confirmation", activeLi: "Panier"});
 });
 
 
